1e6f96993d112a4ed357ad7c5adee1a8af506d4c
Reuben <reubendawemarketing@gmail.com>
2025-06-14 13:13:45 +0100

    Privacy policy footer option created

--- app/public/wp-content/themes/fictional-university-theme/footer.php
+++ app/public/wp-content/themes/fictional-university-theme/footer.php
@@ -1,9 +1,12 @@
 <footer class="site-footer">
+
       <div class="site-footer__inner container container--narrow">
+
         <div class="group">
+
           <div class="site-footer__col-one">
             <h1 class="school-logo-text school-logo-text--alt-color">
-              <a href="#"><strong>Fictional</strong> University</a>
+              <a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
             </h1>
             <p><a class="site-footer__link" href="#">555.555.5555</a></p>
           </div>
@@ -11,9 +14,9 @@
           <div class="site-footer__col-two-three-group">
             <div class="site-footer__col-two">
               <h3 class="headline headline--small">Explore</h3>
-              <nav class="nav-list">
+              <nav class="nav-list min-list">
                 <ul>
-                  <li><a href="#">About Us</a></li>
+                  <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
                   <li><a href="#">Programs</a></li>
                   <li><a href="#">Events</a></li>
                   <li><a href="#">Campuses</a></li>
