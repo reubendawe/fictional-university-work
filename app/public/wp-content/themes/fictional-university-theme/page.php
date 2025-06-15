0d54ab567e82de84c1da6be8accbf558f0e5d73b
Reuben <reubendawemarketing@gmail.com>
2025-06-15 16:37:25 +0100

    Parent & child pages - PHP dynamic text added to only apply options to parent page

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -19,14 +19,16 @@ while(have_posts()) {
     <!-- If statement example -->
     <!-- Gets the ID of the current page I'm viewing, Wordpress uses that number to look up the ID of its parent page -->
       <?php 
-      echo wp_get_post_parent_id(get_the_ID());
-      ?>
-
+      if (wp_get_post_parent_id(get_the_ID())) { ?>
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
           <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span>
         </p>
       </div>
+      <?php }
+      ?>
+
+      
 
       <!-- <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
