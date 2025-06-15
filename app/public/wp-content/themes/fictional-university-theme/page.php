f174901a3572df9b0bb45ac455fd0a978a1ccd33
Reuben <reubendawemarketing@gmail.com>
2025-06-15 16:49:00 +0100

    Parent & child pages - Complete - On to 'To echo or not to echo'

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -19,8 +19,8 @@ while(have_posts()) {
     <!-- If statement example -->
     <!-- Gets the ID of the current page I'm viewing, Wordpress uses that number to look up the ID of its parent page -->
       <?php 
-      $theParent = wp_get_post_parent_id(get_the_ID());
-      if ($theParent) { ?>
+      $theParent = wp_get_post_parent_id(get_the_ID());  // New variable created
+      if ($theParent) { ?> 
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
           <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
