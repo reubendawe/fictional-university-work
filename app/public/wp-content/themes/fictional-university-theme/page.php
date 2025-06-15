0af43fa01c948fb6f0d3b7de93894ecf8cfc13b7
Reuben <reubendawemarketing@gmail.com>
2025-06-15 16:42:56 +0100

    Parent & child pages - Dynamic PHP added to href and title - placeholder 'x' only

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -22,7 +22,7 @@ while(have_posts()) {
       if (wp_get_post_parent_id(get_the_ID())) { ?>
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink(x); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title(x); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
         </p>
       </div>
       <?php }
