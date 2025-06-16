47bf45ad4a222c3033dd368834bb9df34f52a5bd
Reuben <reubendawemarketing@gmail.com>
2025-06-16 16:49:02 +0100

    To echo or not to echo complete - On to Menu of Child Page Links

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,19 +1,5 @@
 <?php get_header(); ?>
 
-<?php
-
-function doubleMe($x) {
-  return $x * 2;
-}
-
-function tripleMe($x) {
-  return $x * 3;
-}
-
-echo tripleMe(doubleMe(5)) // 30
-
-?>
-
 <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg'); ?>)"></div> <!-- Generates Wordpress path to the image --> 
       <div class="page-banner__content container t-center c-white">
