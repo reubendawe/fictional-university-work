d199bf2b78a5f3bb85f3c153155f6b4a193eaa00
Reuben <reubendawemarketing@gmail.com>
2025-07-01 13:24:24 +0100

    Blog Listing Page (index.php vs front-page.php) - Headline and permalink styling

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -17,7 +17,7 @@ get_header(); ?>
     while(have_posts()) {
       the_post(); ?>
       <div class="post-item">
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
         <div class="metabox">
           <p>Posted by Reuben on 01/07/25 in News</p>
@@ -25,7 +25,7 @@ get_header(); ?>
 
         <div class="generic-content">
           <?php the_excerpt(); ?>
-          <p><a href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+          <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
         </div>
 
       </div>
