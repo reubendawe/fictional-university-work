df5425289b4929fe7af267af1173c36f6660f973
Reuben <reubendawemarketing@gmail.com>
2025-07-02 08:27:08 +0100

    Blog Continued - Working metabox added

--- app/public/wp-content/themes/fictional-university-theme/single.php
+++ app/public/wp-content/themes/fictional-university-theme/single.php
@@ -16,8 +16,11 @@ while(have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-        metabox will go here 
-
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main"><?php the_title(); ?></span>
+        </p>
+      </div>
         <div class="generic-content"><?php the_content(); ?></div>
     </div>
 
