858d02a89b70d8fdf4fb0ef3ba9901f67170c754
Reuben <reubendawemarketing@gmail.com>
2025-07-16 13:05:32 +0100

    Code from functions.php cut and pasted into mu-plugins php file

---
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,11 @@
+function university_post_types() {
+    register_post_type('event', array(
+        'public' => true,
+        'labels' => array(
+            'name' => 'Events'
+        ),
+        'menu_icon' => 'dashicons-calendar-alt'
+    ));
+}
+
+add_action('init', 'university_post_types');
