bac48828b37674f8e3c9d899356c8995ba56ad14
Reuben <reubendawemarketing@gmail.com>
2025-07-16 13:17:00 +0100

    'Add Post' under 'Events' on back end changed to 'Add New Event'

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -4,7 +4,8 @@ function university_post_types() {
     register_post_type('event', array(
         'public' => true,
         'labels' => array(
-            'name' => 'Events'
+            'name' => 'Events',
+            'add_new_item' => 'Add New Event'
         ),
         'menu_icon' => 'dashicons-calendar-alt'
     ));
