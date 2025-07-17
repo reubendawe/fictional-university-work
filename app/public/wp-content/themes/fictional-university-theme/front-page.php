e4960d2d94b50da9d2ed8e036a088433cf25faad
Reuben <reubendawemarketing@gmail.com>
2025-07-17 13:23:12 +0100

    While loop looking inside variable, calling in method named 'have_posts()'

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -16,7 +16,14 @@
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
           <?php
-          $homepageEvents = new WP_Query();
+          $homepageEvents = new WP_Query(array(
+            'posts_per_page' => 2,
+            'post_type' => 'event'
+          ));
+
+          while($homepageEvents->have_posts()) {
+            $homepageEvents->the_post();
+          }
           ?>
 
           <div class="event-summary">
