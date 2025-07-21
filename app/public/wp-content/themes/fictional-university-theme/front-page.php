0726e1aea71ab9603f29f4c85c476da0258e9b6f
Reuben <reubendawemarketing@gmail.com>
2025-07-21 16:28:14 +0100

    31. Quick Timeout: Misc Updates - Updated 'View all events' button to be dynamic

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -40,7 +40,8 @@
           <?php }
           ?>
 
-          <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+          <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
+
         </div>
       </div>
       <div class="full-width-split__two">
