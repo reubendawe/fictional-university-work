54367699393bf618c90517c5470cbd0cdef1289e
Reuben <reubendawemarketing@gmail.com>
2025-07-25 16:37:57 +0100

    34. Ordering (Sorting) Custom Queries - Meta queries - Added 'x' as placeholders

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -21,7 +21,14 @@
             'post_type' => 'event',
             'meta_key' => 'event_date',
             'orderby' => 'meta_value_num',
-            'order' => 'ASC'
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => x,
+                'compare' => x,
+                'value' => x,
+              )
+            )
           ));
 
           while($homepageEvents->have_posts()) {
